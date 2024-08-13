427a5d617cc7b8a9e9c092fa9d283158cf1fcb25
Mambo <kdevkr@gmail.com>
2024-08-14 07:05:57 +0900

    rename: ValidDemo to Application
    - Use 7070 port
    - Startup banner off

--- src/main/java/kr/kdev/demo/Application.java
+++ src/main/java/kr/kdev/demo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ValidDemoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ValidDemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
