fdd2eb7e8ec7bad453ddc587f41dba77ac06075a
Mambo <kdevkr@gmail.com>
2024-08-14 23:32:47 +0900

    feat: health check
    - Add health endpoint
    - Test using rest-assured

--- src/main/java/kr/kdev/demo/Application.java
+++ src/main/java/kr/kdev/demo/Application.java
@@ -1,13 +1,31 @@
 package kr.kdev.demo;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@RestController
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/health")
+    public Integer health(HttpServletRequest request,
+                          @RequestHeader(HttpHeaders.USER_AGENT) String userAgent) {
+        // NOTE: Supports forward proxy headers in the spring framework.
+        String remoteAddr = request.getRemoteAddr();
+
+        log.info("health check from {} with {}", remoteAddr, userAgent);
+        return 200;
     }
 
 }
--- src/test/java/kr/kdev/demo/ApplicationTests.java
+++ src/test/java/kr/kdev/demo/ApplicationTests.java
@@ -1,13 +1,43 @@
 package kr.kdev.demo;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
-@SpringBootTest
+@DisplayName("Application Tests")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
 
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.port = port;
+    }
+
+    @DisplayName("Health Check")
     @Test
-    void contextLoads() {
+    void givenProxyHeader_whenHealthCheck_thenOk() {
+        RestAssured.given()
+                .header(new Header("X-Forwarded-For", "173.194.0.0"))
+
+                .when()
+                .log().all()
+                .get("/health")
+
+                .then()
+                .log().ifValidationFails()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(ContentType.JSON)
+                .body(Matchers.is("200"));
     }
 
 }
